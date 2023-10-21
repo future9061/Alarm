ccf17c14c81f2b7f4c591f2d2c9ecdd489d53518
leemirae <416homin@daum.net>
2023-10-22 00:53:03 +0900

    시계  구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Menu from './components/Menu/Menu';
-import { useEffect, useState } from 'react';
-import axios from "axios"
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 
 //react-query에서 refetchInterval으로 주기적으로 1초마다 서버에서 timestamp 가져옴
@@ -12,21 +9,7 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 //usememo내에서 mutate 사용
 
 
-
 function App() {
-  const [watch, setWatch] = useState()
-
-  const { data } = useQuery({
-    queryKey: 'currentTime',
-    queryFn: () => axios.get("/api/time")
-      .then((res) => res.data.success && setWatch(res.data.time))
-      .catch((err) => console.log(err)),
-    refetchInterval: 1000,
-    staletime: 1000, //갱신시간지연시켜서 네트워크 요청 줄이기 기본값 0
-    cachetime: 1000
-  })
-
-
   // const postTime = () => {
   //   return axios.post("/api/time", { timeData })
   //     .then((res) => { res.data.success && console.log("통신 성공") })
--- client/src/components/CurrentTime/CurrentTime.jsx
+++ client/src/components/CurrentTime/CurrentTime.jsx
@@ -1,19 +1,37 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "../../style/CurrentTime/CurrentTimeCSS.scss";
-import { useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 function CurrentTime() {
-  const cache = useQueryClient();
-  useEffect(() => {
-    console.log(cache);
-  }, []);
+  const fetchTime = async () => {
+    return await axios
+      .get("/api/time")
+      .then((res) => {
+        if (res.data.success) {
+          return res.data.time;
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const { data } = useQuery({
+    queryKey: ["currentTime"],
+    queryFn: () => fetchTime(),
+    refetchInterval: 1000,
+    staletime: 1000,
+    cachetime: 1000,
+  });
+
+  const today = data && `${data.year}년 ${data.month}월 ${data.day}일`;
+  const time = data && `${data.hour}시 ${data.minute}분 ${data.second}초`;
 
   return (
     <div className="CurrentTime">
       <div className="inner">
         <h3>현재시각</h3>
-        <p>20:20:20</p>
-        <small>2023년 10월 20일 금요일</small>
+        <p>{today}</p>
+        <small>{time}</small>
       </div>
     </div>
   );
